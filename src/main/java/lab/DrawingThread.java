c602c004daa3a6ce85534ca7f51ac71a79fd3c3b
prezmyl <prezmyl@gmail.com>
2025-01-01 21:01:33 +0100

    kolize predelane, default metoda v interface, individualni chovani v objektech jednotlive, stoji is/is not active, vymazany nepouzivany kod

--- src/main/java/lab/DrawingThread.java
+++ src/main/java/lab/DrawingThread.java
@@ -41,20 +41,9 @@ public class DrawingThread extends AnimationTimer {
 			gameObject.add(new Barricade(200 + i * 100, 300));
 		}
 
-		/*if (player.getHealth().getLives() <= 0){
-			onGameOver();
-		}*/
-
-
-
 	}
 
-	/*private void onGameOver() {
-		stop();
-		System.out.println("onGameOver, stopping simulation!");
-		Platform.runLater(() -> gameStateObserver.onGameOver());
 
-	}*/
 
 	@Override
 	public void handle(long now) {
@@ -79,19 +68,6 @@ public class DrawingThread extends AnimationTimer {
 				.map(obj -> (DrawAble) obj)
 				.forEach(drawable -> drawable.draw(gc));
 
-
-	/*	gameObject.stream()
-					.filter(obj -> obj instanceof Collisionable)
-					.map(obj -> (Collisionable) obj)
-					.forEach(col1 -> gameObject.stream()
-						.filter(obj -> obj instanceof Collisionable)
-						.map(obj -> (Collisionable) obj)
-						.filter(col2 -> col1 != col2)
-						.filter(col2 -> col1.isActive() && col2.isActive())
-						.filter(col2 -> col1.intersect(col2.getBoundingBox()))
-						.forEach(col2 -> col1.handleCollision(col2)));
-*/
-
 		gameStateObserver.onScoreUpdate(scoreManager.getScore());
 		gameStateObserver.onLivesUpdate(player.getHealth().getLives());
 
@@ -104,123 +80,26 @@ public class DrawingThread extends AnimationTimer {
 
 	}
 
-/*
-private void checkCollisions() {
-    List<Collisionable> activeObjects = gameObject.stream()
-            .filter(obj -> obj instanceof Collisionable)
-            .map(obj -> (Collisionable) obj)
-            .filter(Collisionable::isActive)
-            .toList();
+	private  void checkCollisions(){
+			List<Collisionable> activeObjects = gameObject.stream()
+					.filter(obj -> obj instanceof Collisionable)
+					.map(obj -> (Collisionable) obj)
+					.filter(Collisionable::isActive)
+					.toList();
 
-    // Set pro sledování zpracovaných kolizí
-    Set<Pair<Collisionable, Collisionable>> processedCollisions = new HashSet<>();
+			for (int i = 0; i < activeObjects.size(); i++) {
+				for (int j = i + 1; j < activeObjects.size(); j++) {
+					Collisionable col1 = activeObjects.get(i);
+					Collisionable col2 = activeObjects.get(j);
 
-    for (int i = 0; i < activeObjects.size(); i++) {
-        for (int j = i + 1; j < activeObjects.size(); j++) {
-            Collisionable col1 = activeObjects.get(i);
-            Collisionable col2 = activeObjects.get(j);
-
-            Pair<Collisionable, Collisionable> collisionPair = new Pair<>(col1, col2);
-
-            // Pokud již byla tato kolize zpracována, přeskočíme
-            if (processedCollisions.contains(collisionPair)) {
-                continue;
-            }
-
-            col1.handleCollision(col2);
-            processedCollisions.add(collisionPair);
-        }
-    }
-}
-
-*/	private  void checkCollisions(){
-		List<Collisionable> activeObjects = gameObject.stream()
-				.filter(obj -> obj instanceof Collisionable)
-				.map(obj -> (Collisionable) obj)
-				.filter(Collisionable::isActive)
-				.toList();
-
-		for (int i = 0; i < activeObjects.size(); i++) {
-			for (int j = i + 1; j < activeObjects.size(); j++) {
-				Collisionable col1 = activeObjects.get(i);
-				Collisionable col2 = activeObjects.get(j);
-
-				col1.handleCollision(col2);
-			}
-		}
-	}
-/*
-	private void checkCollisions() {
-		for (int i = 0; i < gameObject.size(); i++) {
-			GameObject obj1 = gameObject.get(i);
-			if (!(obj1 instanceof Collisionable)) {
-				continue;
-			}
-			for (int j = i + 1; j < gameObject.size(); j++) {
-				GameObject obj2 = gameObject.get(j);
-				if (!(obj2 instanceof Collisionable)) {
-					continue;
-				}
-
-				Collisionable col1 = (Collisionable) obj1;
-				Collisionable col2 = (Collisionable) obj2;
-
-				// Kontrola, zda jsou oba objekty aktivni
-				if (!col1.isActive() || !col2.isActive()) {
-					continue;
-				}
-
-				if (col1.intersect(col2.getBoundingBox())) {
-					col1.hitBy(col2);
-					col2.hitBy(col1);
-
-					// Zpracujeme kolizi pouze jednou
-					if (obj1 instanceof Enemy && obj2 instanceof Player) {
-						handlePlayerEnemyCollision((Player) obj2, (Enemy) obj1);
-					} else if (obj2 instanceof Enemy && obj1 instanceof Player) {
-						handlePlayerEnemyCollision((Player) obj1, (Enemy) obj2);
-					}
-
-					if (obj1 instanceof Bullet && obj2 instanceof Enemy) {
-						handleBulletEnemyCollision((Bullet) obj1, (Enemy) obj2);
-					} else if (obj2 instanceof Bullet && obj1 instanceof Enemy) {
-						handleBulletEnemyCollision((Bullet) obj2, (Enemy) obj1);
-					}
+					col1.handleCollision(col2);
 				}
 			}
-		}
-	}*/
-
-
-	private void handlePlayerEnemyCollision(Player player, Enemy enemy) {
-		if (!enemy.isActive()) {
-			return; // Zabrání opakovanému zpracování stejné kolize
-		}
-
-		player.getHealth().decreaseLives();
-		System.out.println("Player lives decreased. Current lives: " + player.getHealth().getLives()); // Debug výpis
-		enemy.setActive(false);
-
-
 	}
 
-	private void handleBulletEnemyCollision(Bullet bullet, Enemy enemy) {
-		if (enemy.isActive()) {
-			System.out.println("Handling collision: Bullet -> Enemy");
-			enemy.setActive(false);
-			bullet.setActive(false);
 
-			if (scoreManager != null) {
-				scoreManager.increaseScore(100);
-				System.out.println("Score increased. Current score: " + scoreManager.getScore());
-			} else {
-				System.out.println("ScoreManager is null!");
-			}
-		}
+	public void addBullet(Bullet bullet) {
+		gameObject.add(bullet);
 	}
 
-	// Pridani nove strely
-		public void addBullet(Bullet bullet) {
-			gameObject.add(bullet);
-		}
 }
